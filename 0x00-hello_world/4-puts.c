51eaf7e256fc548fb4de6ba1e4a1275173142c8a
MokoenaD <dkleelayla79@gmail.com>
2023-07-13 10:14:51 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+sinclude <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(voidi)
+(
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+)
